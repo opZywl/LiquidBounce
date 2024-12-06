b5e5d18593cd7a27a64b4ae1c62ea8c9ae168763
mems01 <70612656+mems01@users.noreply.github.com>
2024-12-06 18:59:18 +0200

    feat(legacy): Breadcrumbs with line height support. (#4813)

---
+++ src/main/java/net/ccbluex/liquidbounce/utils/extensions/ColorExtensions.kt
@@ -0,0 +1,10 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.utils.extensions
+
+import java.awt.Color
+
+fun Color.withAlpha(a: Int) = Color(red, green, blue, a)
