8b3c5da87e3cb17fe85a7e608b72a92c25f8b2e0
mems01 <70612656+mems01@users.noreply.github.com>
2024-12-12 03:20:39 +0200

    fixes(legacy): FakeLag sometimes messing up general packet order when receiving knock-back / Blocks being barely visible in NameTags. (#4864)
    By eliminating even more packet-order mess up cases, FakeLag now becomes 99% perfect.

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/NameTags.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/NameTags.kt
@@ -5,6 +5,7 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.render
 
+import net.ccbluex.liquidbounce.config.*
 import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.event.Render3DEvent
 import net.ccbluex.liquidbounce.features.module.Category
@@ -14,11 +15,7 @@ import net.ccbluex.liquidbounce.ui.font.Fonts
 import net.ccbluex.liquidbounce.utils.attack.EntityUtils.getHealth
 import net.ccbluex.liquidbounce.utils.attack.EntityUtils.isLookingOnEntities
 import net.ccbluex.liquidbounce.utils.attack.EntityUtils.isSelected
-import net.ccbluex.liquidbounce.utils.rotation.RotationUtils.isEntityHeightVisible
 import net.ccbluex.liquidbounce.utils.extensions.*
-import net.ccbluex.liquidbounce.utils.extensions.getPing
-import net.ccbluex.liquidbounce.utils.extensions.interpolatedPosition
-import net.ccbluex.liquidbounce.utils.extensions.lastTickPos
 import net.ccbluex.liquidbounce.utils.render.ColorUtils
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.disableGlCap
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.drawTexturedModalRect
@@ -26,9 +23,10 @@ import net.ccbluex.liquidbounce.utils.render.RenderUtils.enableGlCap
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.quickDrawBorderedRect
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.quickDrawRect
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.resetCaps
-import net.ccbluex.liquidbounce.config.*
+import net.ccbluex.liquidbounce.utils.rotation.RotationUtils.isEntityHeightVisible
 import net.minecraft.client.entity.EntityPlayerSP
 import net.minecraft.client.renderer.GlStateManager.*
+import net.minecraft.client.renderer.RenderHelper
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
@@ -305,6 +303,7 @@ object NameTags : Module("NameTags", Category.RENDER, hideModule = false) {
         }
 
         if (armor && entity is EntityPlayer) {
+            RenderHelper.enableGUIStandardItemLighting()
             for (index in 0..4) {
                 val itemStack = entity.getEquipmentInSlot(index) ?: continue
 
@@ -313,6 +312,7 @@ object NameTags : Module("NameTags", Category.RENDER, hideModule = false) {
                     itemStack, -50 + index * 20, if (potion && foundPotion) -42 else -22
                 )
             }
+            RenderHelper.disableStandardItemLighting()
 
             enableAlpha()
             disableBlend()
