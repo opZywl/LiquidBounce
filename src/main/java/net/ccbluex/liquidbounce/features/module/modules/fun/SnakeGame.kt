780645ddb53a7b0301f0093d7f9843bc0a29ebf0
Zywl <86253343+opZywl@users.noreply.github.com>
2025-01-12 20:48:35 -0300

    move category

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/SnakeGame.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/SnakeGame.kt
@@ -3,7 +3,7 @@
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
  * https://github.com/CCBlueX/LiquidBounce/
  */
-package net.ccbluex.liquidbounce.features.module.modules.render
+package net.ccbluex.liquidbounce.features.module.modules.fun
 
 import net.ccbluex.liquidbounce.config.choices
 import net.ccbluex.liquidbounce.event.KeyEvent
